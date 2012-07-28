e314f4cd3c73bcb9ff57ecd27a73f61e65aff6f0
charlie-rudenstal <charlie4@gmail.com>
2012-07-28 19:53:04 +0200

    Added test folder

---
+++ test/src/Math.test.js
@@ -0,0 +1,17 @@
+/* Copyright (c) 2012 Charlie Rudenstål */
+/*global require, describe, it */
+var should = require('chai').should(); 
+var MathLib = require("../../public/src/Helpers/MathLib");
+
+describe('MathLib', function() {
+
+  describe('#add()', function() {
+
+    it('should return the sum of its two parameters', function(){
+      var math = new MathLib();
+      math.add(2, 16).should.equal(18);
+    });
+
+  });
+
+});
