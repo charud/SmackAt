fb16dbb097b4f647eea257f539210657640845e7
charlie-rudenstal <charlie4@gmail.com>
2012-08-05 00:03:06 +0200

    Temporarily removed socketio from next due to conflict with www

--- server.js
+++ server.js
@@ -3,11 +3,11 @@
 
 var express = require("express");
 var http = require("http");
-var socketio = require("socket.io");
+//var socketio = require("socket.io");
 
 var app = express();
 var server = http.createServer(app);
-var io = socketio.listen(server);
+//var io = socketio.listen(server);
 
 // Serve the public directory
 app.use(express.static(__dirname + "/public"));
